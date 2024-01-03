e1a19b7f4008af3f2293f51900c89aaa00816876
Ben Kimani <benzoic09@gmail.com>
2024-01-03 03:45:43 -0800

    task one

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,12 +22,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->right = NULL;
-	/*new_node->left = NULL;*/
+	new_node->left = NULL;
 
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
-		parent->left->parent = new_node;
+		new_node->left->parent = new_node;
 	}
 	parent->left = new_node;
 	return (new_node);
