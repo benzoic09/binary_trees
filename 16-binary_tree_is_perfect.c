00ba0eed681935d2adc0c95b356c41f2f80f4293
Ben Kimani <benzoic09@gmail.com>
2024-01-04 14:27:04 -0800

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,10 +13,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	
+
 	return ((left_height > right_height) ?
 			(left_height + 1) : (right_height + 1));
 }
