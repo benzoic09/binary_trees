a7f970addf137c50aec1795eed6ba848603dcfb7
Ben Kimani <benzoic09@gmail.com>
2024-01-04 05:27:47 -0800

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,10 +32,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 	size_t left_height, right_height;
 
 	if (tree->left == NULL && tree->right == NULL)
-		 return 0;
+		return (0);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return (int)(left_height - right_height);
+	return ((int)(left_height - right_height));
 }
